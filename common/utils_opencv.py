608df25d4afafa58aff9a191bbc9b18f3e40f079
aestaq <atinderpalap@gmail.com>
2018-02-08 03:44:06 +0530

    Buffersize not working for opencv2 on tx1, so removing it for opencv2

--- common/utils_opencv.py
+++ common/utils_opencv.py
@@ -9,7 +9,8 @@ FACE_PAD = 50
 class VideoStream(object):
     def __init__(self, url, queueSize=4):
         self.stream = cv2.VideoCapture(url)
-        self.stream.set(cv2.CAP_PROP_BUFFERSIZE,3)
+        if cv2_version == '3':
+            self.stream.set(cv2.CAP_PROP_BUFFERSIZE,3)
         self.stopped = False
         self.frameBuffer = Queue(maxsize=queueSize)
 
