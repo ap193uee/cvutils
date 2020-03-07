a20b2cadb58c5c9fefdec6eb0c7a2d619bf26e2d
ashwin20 <ashwin20@gitlab.in>
2020-03-07 17:14:32 +0530

    added number of workers

--- common/performance.py
+++ common/performance.py
@@ -1,10 +1,12 @@
 class Multicore(object):
-    def __init__(self, func):
+    def __init__(self, func ,num_workers=None):
         from multiprocessing import Pool, cpu_count
         from collections import deque
         from time import sleep
-
-        self.processn = cpu_count()
+        if num_workers is None or num_workers > cpu_count():
+            self.processn = cpu_count()
+        else:
+            self.processn = num_workers
         self.pool = Pool(processes = self.processn)
         self.pending = deque()
         self.func = func
