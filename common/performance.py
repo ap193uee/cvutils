60ced7e06630771309d54bee777354841af8994d
saurabh1993 <saurabh1993.kumar@gmail.com>
2020-10-05 12:02:48 +0530

    Update performance.py

--- common/performance.py
+++ common/performance.py
@@ -3,7 +3,7 @@ class Multicore(object):
         from multiprocessing import Pool, cpu_count
         from collections import deque
         from time import sleep
-        if num_workers is None or num_workers > cpu_count():
+        if num_workers is None:
             self.processn = cpu_count()
         else:
             self.processn = num_workers
