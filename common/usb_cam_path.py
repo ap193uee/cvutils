2e6cebe05e63c582c9c317acb69670b8a903ae97
saurabh <saurabhk@vedalabs.in>
2020-10-20 16:04:30 +0530

    update usb cam finder with serial_id and model_id

--- common/usb_cam_path.py
+++ common/usb_cam_path.py
@@ -2,22 +2,46 @@ import subprocess
 
 import sys
 
-def find_usb_cam_path(vendor_id):
+def get_ids(res_ids):
+#    print("here",res_ids)
+    ids=[]
+    for res in res_ids:
+        index = res.find('=')
+        id=res[index+1:]
+        ids.append(id)
+    return ids[0],ids[1],ids[2]
+
+def find_usb_cam_path(**kwargs):
+    
+    # Get all the provided ids
+    VENDOR_ID=kwargs.get('VENDOR_ID',None)
+    MODEL_ID=kwargs.get('MODEL_ID',None)
+    SERIAL_ID=kwargs.get('SERIAL_ID',None)
+    print(VENDOR_ID,MODEL_ID,SERIAL_ID)
+    match_pattern='VENDOR_ID\|MODEL_ID\|SERIAL_SHORT'
+]
+    # Loop through camera mount paths and find the matching ids
     for num in range (0,10):
         cam_path = '/dev/video'+str(num)
         try:
             ps = subprocess.Popen(['udevadm', 'info', '--query=all',cam_path], stdout=subprocess.PIPE)
-            output = subprocess.check_output(('grep', 'VENDOR_ID'), stdin=ps.stdout)
+            output = subprocess.check_output(('grep', match_pattern), stdin=ps.stdout)
             ps.wait()
-            res=output.decode('utf-8').split('\n')[0]
-            index = res.find('=')
-            res=res[index+1:]
-            print("res",res)
-            if res == vendor_id:
+            res=output.decode('utf-8').split('\n')
+            mid,sid,vid=get_ids(res)
+            print(mid,sid,vid)
+            if SERIAL_ID is not None and sid==SERIAL_ID:
                 print("cam path found", cam_path)
                 return cam_path
-                
-        except:
-            pass
+            if MODEL_ID is not None and mid==MODEL_ID:
+                print("cam path found", cam_path)
+                return cam_path
+            if VENDOR_ID is not None and vid==VENDOR_ID:
+                print("cam path found", cam_path)
+                return cam_path
+
+        except Exception as e:
+            print(e)
     print("no cam path found")
     return None
+            
